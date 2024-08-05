bc992e8b3c33ccf852d137f9e10500088dfad8a1
Urcodingbuddy <anshpethe110@gmail.com>
2024-08-06 00:27:31 +0530

    VideoCards Done

--- youtube-clone/src/components/VideoCard.tsx
+++ youtube-clone/src/components/VideoCard.tsx
@@ -1,10 +1,12 @@
 export function VideoCard(props:any){
     return <div>
         <img className="rounded-xl w-13 h-13 " src={props.thumbImage} alt="" />
-        <div className="grid pt-2">
-                <img className="col-span-1 rounded-full w-10 h-10" src={props.pfpImage} alt="" />
-            <div className="col-span-11 pl-0">
-                <div>{props.title}</div>
+        <div className="grid detailgrid pt-2">
+            <div className="grid col-span-2" >
+                <img className="inline col-span-1 rounded-full w-10 h-10" src={props.pfpImage} alt="" />
+            </div>
+            <div className="col-span-12">
+                <div className="inline" >{props.title}</div>
                 <div className="flex flex-row   items-center gap-1 text-gray-300" >{props.chName} <img className="inline " src="/assets/varified.svg" alt="" /></div>
                 <span className="col-span-11 text-gray-300 text-base " >{props.views}</span>
                 <span className="col-span-11 text-base " >・</span>
@@ -12,11 +14,5 @@ export function VideoCard(props:any){
                      
             </div>
         </div>
-    </div>
-}
-
-function VideoCard2(){
-    return <div>
-
     </div>
 }
