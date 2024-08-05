34368f6cb04dc6079a2be3145e653d7283857901
Urcodingbuddy <anshpethe110@gmail.com>
2024-08-06 00:29:24 +0530

    video card and grid embeded

--- youtube-clone/src/pages/index.tsx
+++ youtube-clone/src/pages/index.tsx
@@ -1,21 +1,13 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import {VideoCard} from "../components/VideoCard";
+import {VideoGrid} from "../components/VideoGrid";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
       <div>
-        <VideoCard
-        thumbImage={"https://i.postimg.cc/MphsXFG1/image.png"}
-        pfpImage={"https://i.postimg.cc/pr7Y9mBq/image.jpg"}
-        title={"Natrure: Dolby Vision(4k) | The Most Beautiful Earth 4K HDR 60 FPS"}
-        chName={"8K VISON"}
-        views={"500K views"}
-        uDate={"1 month ago"}
-        >  
-        </VideoCard>
+        <VideoGrid/>
       </div>
   )
 }
