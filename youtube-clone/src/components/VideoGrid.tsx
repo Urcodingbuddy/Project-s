92c2eb45d8e4e7434f8ecbb3c2773a10fd1c1dec
Urcodingbuddy <anshpethe110@gmail.com>
2024-08-06 02:09:13 +0530

    VideoGrid Desiged

--- youtube-clone/src/components/VideoGrid.tsx
+++ youtube-clone/src/components/VideoGrid.tsx
@@ -31,6 +31,70 @@ const Videos = [
         chName: "8K VISON",
         views: "500K views",
         uDate: "1 month ago"
+    },
+    {
+        thumbImage: "https://i.postimg.cc/MphsXFG1/image.png",
+        pfpImage: "https://i.postimg.cc/pr7Y9mBq/image.jpg",
+        title: "Natrure: Dolby Vision(4k) | The Most Beautiful Earth 4K HDR 60 FPS",
+        chName: "8K VISON",
+        views: "500K views",
+        uDate: "1 month ago"
+    },
+    {
+        thumbImage: "https://i.postimg.cc/MphsXFG1/image.png",
+        pfpImage: "https://i.postimg.cc/pr7Y9mBq/image.jpg",
+        title: "Natrure: Dolby Vision(4k) | The Most Beautiful Earth 4K HDR 60 FPS",
+        chName: "8K VISON",
+        views: "500K views",
+        uDate: "1 month ago"
+    },
+    {
+        thumbImage: "https://i.postimg.cc/MphsXFG1/image.png",
+        pfpImage: "https://i.postimg.cc/pr7Y9mBq/image.jpg",
+        title: "Natrure: Dolby Vision(4k) | The Most Beautiful Earth 4K HDR 60 FPS",
+        chName: "8K VISON",
+        views: "500K views",
+        uDate: "1 month ago"
+    },
+    {
+        thumbImage: "https://i.postimg.cc/MphsXFG1/image.png",
+        pfpImage: "https://i.postimg.cc/pr7Y9mBq/image.jpg",
+        title: "Natrure: Dolby Vision(4k) | The Most Beautiful Earth 4K HDR 60 FPS",
+        chName: "8K VISON",
+        views: "500K views",
+        uDate: "1 month ago"
+    },
+    {
+        thumbImage: "https://i.postimg.cc/MphsXFG1/image.png",
+        pfpImage: "https://i.postimg.cc/pr7Y9mBq/image.jpg",
+        title: "Natrure: Dolby Vision(4k) | The Most Beautiful Earth 4K HDR 60 FPS",
+        chName: "8K VISON",
+        views: "500K views",
+        uDate: "1 month ago"
+    },
+    {
+        thumbImage: "https://i.postimg.cc/MphsXFG1/image.png",
+        pfpImage: "https://i.postimg.cc/pr7Y9mBq/image.jpg",
+        title: "Natrure: Dolby Vision(4k) | The Most Beautiful Earth 4K HDR 60 FPS",
+        chName: "8K VISON",
+        views: "500K views",
+        uDate: "1 month ago"
+    },
+    {
+        thumbImage: "https://i.postimg.cc/MphsXFG1/image.png",
+        pfpImage: "https://i.postimg.cc/pr7Y9mBq/image.jpg",
+        title: "Natrure: Dolby Vision(4k) | The Most Beautiful Earth 4K HDR 60 FPS",
+        chName: "8K VISON",
+        views: "500K views",
+        uDate: "1 month ago"
+    },
+    {
+        thumbImage: "https://i.postimg.cc/MphsXFG1/image.png",
+        pfpImage: "https://i.postimg.cc/pr7Y9mBq/image.jpg",
+        title: "Natrure: Dolby Vision(4k) | The Most Beautiful Earth 4K HDR 60 FPS",
+        chName: "8K VISON",
+        views: "500K views",
+        uDate: "1 month ago"
     }
 ]
 
--- youtube-clone/src/pages/index.tsx
+++ youtube-clone/src/pages/index.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import {VideoGrid} from "../components/VideoGrid";
+import { AppBar } from "@/components/Appbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
       <div>
+        <AppBar/>
         <VideoGrid/>
       </div>
   )
