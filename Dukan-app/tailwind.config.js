68efe5145608e4612a237fa5f3eacbd4eb2738be
Urcodingbuddy <anshpethe110@gmail.com>
2024-08-08 21:10:52 +0530

    Added custom colors

---
+++ Dukan-app/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        700:"#146eb4",
+        101:"#0e4f82"
+      }
+    },
+  },
+  plugins: [],
+}
