c12d43f2d50df2fe01882773559b487e24a726ed
Urcodingbuddy <anshpethe110@gmail.com>
2024-08-08 21:11:44 +0530

    Created multiple Revenue Card

---
+++ Dukan-app/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import './App.css'
+import {RevenueCard} from './components/RevenueCard'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='grid grid-cols-4'>
+
+      <RevenueCard title={"Amout Pending"} amount={"92,523.20"} orderCount={25}/>
+      <RevenueCard title={"Amout Pending"} amount={"92,523.20"} orderCount={25}/>
+      <RevenueCard title={"Amout Pending"} amount={"92,523.20"} orderCount={25}/>
+      <RevenueCard title={"Amout Pending"} amount={"92,523.20"} orderCount={25}/>
+      <RevenueCard title={"Amout Pending"} amount={"92,523.20"} orderCount={25}/>
+      <RevenueCard title={"Amout Pending"} amount={"92,523.20"} orderCount={25}/>
+      <RevenueCard title={"Amout Pending"} amount={"92,523.20"} orderCount={25}/>
+      <RevenueCard title={"Amout Pending"} amount={"92,523.20"} orderCount={25}/>
+      <RevenueCard title={"Amout Pending"} amount={"92,523.20"} orderCount={25}/>
+      <RevenueCard title={"Amout Pending"} amount={"92,523.20"} orderCount={25}/>
+      <RevenueCard title={"Amout Pending"} amount={"92,523.20"} orderCount={25}/>
+      <RevenueCard title={"Amout Pending"} amount={"92,523.20"} orderCount={25}/>
+      <RevenueCard title={"Amout Pending"} amount={"92,523.20"} orderCount={25}/>
+      <RevenueCard title={"Amout Pending"} amount={"92,523.20"} orderCount={25}/>
+      <RevenueCard title={"Amout Pending"} amount={"92,523.20"} orderCount={25}/>
+      <RevenueCard title={"Amout Pending"} amount={"92,523.20"} orderCount={25}/>
+    </div>
+  )
+}
+
+export default App
