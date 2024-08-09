cfb3ff67991aff0414b05d34e3b5454607a48bc2
Urcodingbuddy <anshpethe110@gmail.com>
2024-08-10 00:42:36 +0530

    Schmea and Connection string is defind

---
+++ paytm/backend/db.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+
+
+mongoose.connect("mongodb+srv://anshpethe110:%40mangoanshpethe159@cluster0.702twr0.mongodb.net/paytm-app?retryWrites=true&w=majority")
+
+.then(()=> console.log("Connected to MongoDB"))
+.catch((err)=> console.log("Failed to connect: "+err));
+
+
+const userSchema = new mongoose.Schema({
+    firstName:{
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    },
+    lastName:{
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    },
+    username:{
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        minLength: 3,
+        maxLength: 30
+    },
+    
+    password:{
+        type: String,
+        required: true,
+        minLength: 6
+    },
+})
+
+const accountSchema = new mongoose.Schema({
+    userId:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    balance: {
+        type: Number,
+        required: true
+    }
+})
+
+const Account = mongoose.model('Account', accountSchema);
+const User = mongoose.model("User",userSchema);
+module.exports = {
+    User,
+    Account,
+} 
+
