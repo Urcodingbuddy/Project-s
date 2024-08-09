954f975229ba9ee2cbd46c3b0294d0b42332f7b4
Urcodingbuddy <anshpethe110@gmail.com>
2024-08-10 00:45:04 +0530

    port file for local hosting is created - http://localhost:3000/app/v1/user/singup

--- paytm/backend/index.js
+++ paytm/backend/index.js
@@ -1,3 +1,13 @@
-const express = require("express");
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 3000;
+app.use(cors());
+app.use(express.json()); 
+const mainRouter = require('./routes/index');
 
 
+app.use('/app/v1',mainRouter)
+app.listen(port,'0.0.0.0',()=>{
+    console.log(`Server is running on port ${port}`);
+})
