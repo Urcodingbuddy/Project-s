3abd35d88256d10d3038f80a3a7cfa191eaca73e
Urcodingbuddy <anshpethe110@gmail.com>
2024-08-09 11:19:39 +0530

    changes for storybook

--- Dukan-app/src/App.jsx
+++ Dukan-app/src/App.jsx
@@ -3,8 +3,6 @@ import './App.css'
 import {RevenueCard} from './components/RevenueCard'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className='grid grid-cols-4'>
 
---
+++ Dukan-app/src/stories/component/RevenueCard.stories.jsx
@@ -0,0 +1,24 @@
+import { RevenueCard } from '../../components/RevenueCard';
+
+const meta = {
+  component: RevenueCard,
+};
+
+export default meta;
+
+
+export const BigAmount = {
+  args: {
+    orderCount: "1212",
+    title:"SDG",
+    amount:"9999999999999"
+  },
+};
+
+export const smallAmount = {
+  args: {
+    orderCount: "1212",
+    title:"SDG",
+    amount:"99"
+  },
+};
