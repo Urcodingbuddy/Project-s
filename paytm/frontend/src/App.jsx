1f57ca183ce11a58fc7f77b23c696074956278bb
Urcodingbuddy <anshpethe110@gmail.com>
2024-08-11 02:25:21 +0530

    main file with routes

--- paytm/frontend/src/App.jsx
+++ paytm/frontend/src/App.jsx
@@ -1,10 +1,31 @@
+import {
+  BrowserRouter,
+  Route,
+  Routes
+} from "react-router-dom";
+import { useState } from "react";
+import { Signup } from "./pages/Signup";
+import { Signin } from "./pages/Signin";
+import { Dashboard } from "./pages/Dashboard";
+import { SendMoney } from "./pages/SendMoney";
+import { Landing } from "./pages/Landing";
+import {RecoilRoot} from 'recoil'
+
 
 function App() {
-
   return (
-    <div>
-        Hello world
-    </div>
+    <><RecoilRoot>
+       <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<Signup  />} />
+          <Route path="/signin" element={<Signin  />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/send" element={<SendMoney />} />
+          <Route path="/" element={<Landing/>}/>
+        </Routes>
+      </BrowserRouter>
+      </RecoilRoot>
+    </>
   )
 }
 
